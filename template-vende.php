f29e90e56898eef75e09095c7e1308001a5f2ea3
mespineira <manel.espineira@gmail.com>
2025-09-29 16:27:34 +0200

    añadido de funcionalidad para valorar inmuebles

--- template-vende.php
+++ template-vende.php
@@ -4,6 +4,7 @@ get_header(); ?>
 <div class="ge-container ge-content">
 	<?php ge_breadcrumbs(); ?>
 	<header class="ge-archive-head"><h1><?php esc_html_e('Vende tu inmueble','gandara-estate'); ?></h1></header>
+	<?php the_content(); ?>
 	<p><?php esc_html_e('Cuéntanos sobre tu propiedad y nos pondremos en contacto.','gandara-estate'); ?></p>
 	<?php echo do_shortcode('[rep_contact]'); ?>
 </div>
